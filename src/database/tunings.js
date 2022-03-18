15a2a8f7e77cf115837dd482fc5b8f8b9de85678
Quentin GOLDOVSKY <quentin.goldovsky@sii.fr>
2022-03-18 14:04:37 +0100

    tuning js update

--- src/database/tunings.js
+++ src/database/tunings.js
@@ -13,64 +13,67 @@
  * LEVELS
  * 1 : instrument
  * 2 : nb strings
- * 3 : name
+ * 3 : type of tuning (Standard, drop, open, 4th...)
  * 4 : strings (from high to low)
  *
- * NOTES/TODO
- * change E by E4...
+ * TODO
+ * add different tuning
  */
 
-export default [
-    {
+export default {
       guitar: {
         nb_strings_6: {
-            standard: [ "E4", "B3", "G3", "D3", "A2", "E2" ]
+          standard: {
+            e: [ "E4", "B3", "G3", "D3", "A2", "E2" ],
+            eb: [ "Eb4", "Bb3", "Gb3", "Db3", "Ab2", "Eb2" ],
+            d: [ "D4", "A3", "F3", "C3", "G2", "D2" ],
+            db: [ "Db4", "Ab3", "E3", "B2", "Gb2", "Db2" ],
+            c: [ "C4", "G3", "Eb3", "Bb2", "F2", "C2" ],
+            b: [ "B4", "Gb3", "D3", "A2", "E2", "B1" ],
+            bb: [ "Bb4", "F3", "Db3", "Ab2", "Eb2", "Bb1" ],
+            a: [ "A4", "E3", "C3", "G2", "D2", "A1" ]
+          },
+          drop: {
+            d: [ "E4", "B3", "G3", "D3", "A2", "D2" ],
+            Db: [ "Eb4", "Bb3", "Gb3", "Db3", "Ab2", "Db2" ],
+            c: [ "D4", "A3", "F3", "C3", "G2", "C2" ]
+          },
+          open: {
+            e: [ "E4", "B3", "G#3", "E3", "G2", "E2" ],
+            d: [ "D4", "A3", "F#3", "D3", "A2", "D2" ],
+            c: [ "C4", "G3", "E3", "C3", "G2", "C2" ],
+            ctownsend: [ "E4", "C4", "G3", "C3", "G2", "C2" ],
+          }
         },
-        nb_strings_7: {},
-        nb_strings_8: {}
+        nb_strings_7: {
+          standard: {
+            b: [ "E4", "B3", "G3", "D3", "A2", "E2", "B1" ],
+            bb: [ "Eb4", "Bb3", "Gb3", "Db3", "Ab2", "Eb2", "Bb1" ],
+            a: [ "d4", "A3", "F3", "C3", "G2", "D2", "A1" ],
+          },
+          drop: {
+            a: [ "E4", "B3", "G3", "D3", "A2", "E2", "A1" ],
+          }
+        },
+        nb_strings_8: {
+          standard: {
+            fsharp: [ "E4", "B3", "G3", "D3", "A2", "E2", "B1", "F#1" ]
+          },
+          drop: {
+            e: [ "E4", "B3", "G3", "D3", "A2", "E2", "B1", "E1" ],
+          }
+        }
       },
       bass: {
-        nb_strings_5: {}
+        nb_strings_4: {
+          standard: {
+            e: [ "D2", "A1", "E1", "B0" ],
+          },
+        },
+        nb_strings_5: {
+          standard: {
+            b: [ "G2", "D2", "A1", "E1", "B0" ],
+          },
+        }
       }
-    },
-  ];
-
- /* 
- * NOTE: strings are inverted from the format I wanna use
- [
-    { "type": "guitar",  "name": "Standard",       "strings": [ "E", "A", "D", "G", "B", "E" ] },
-    { "type": "guitar",  "name": "Open A",         "strings": [ "E", "A", "C#", "E", "A", "E" ] },
-    { "type": "guitar",  "name": "Open B",         "strings": [ "B", "F#", "B", "F#", "B", "D#" ] },
-    { "type": "guitar",  "name": "Open C",         "strings": [ "C", "G", "C", "G", "C", "E" ] },
-    { "type": "guitar",  "name": "Open D",         "strings": [ "D", "A", "D", "F#", "A", "D" ] },
-    { "type": "guitar",  "name": "Open D minor",   "strings": [ "D", "A", "D", "F", "A", "D" ] },
-    { "type": "guitar",  "name": "Open E",         "strings": [ "E", "B", "E", "G#", "B", "E" ] },
-    { "type": "guitar",  "name": "Open F",         "strings": [ "C", "F", "C", "F", "A", "C" ] },
-    { "type": "guitar",  "name": "Open G",         "strings": [ "D", "G", "D", "G", "B", "D" ] },
-    { "type": "guitar",  "name": "Half step down", "strings": [ "D#", "G#", "C#", "F#", "A#", "D#" ] },
-    { "type": "guitar",  "name": "Full step down", "strings": [ "D", "G", "C", "F", "A", "D" ] },
-    { "type": "guitar",  "name": "Drop D",         "strings": [ "D", "A", "D", "G", "B", "E" ] },
-    { "type": "guitar",  "name": "Drop C#",        "strings": [ "C#", "G#", "C#", "F#", "A#", "D#" ] },
-    { "type": "guitar",  "name": "Drop C",         "strings": [ "C", "G", "C", "F", "A", "D" ] },
-    { "type": "guitar",  "name": "Drop B",         "strings": [ "B", "F#", "B", "E", "G#", "C#" ] },
-    { "type": "guitar",  "name": "Drop A",         "strings": [ "A", "E", "A", "D", "F#", "B" ] },
-    { "type": "guitar",  "name": "Baritone 4th",   "strings": [ "B", "E", "A", "D", "F#", "B" ] },
-    { "type": "guitar",  "name": "Baritone 5th",   "strings": [ "A", "D", "G", "C", "E", "A" ] },
-  
-    { "type": "guitar",  "name": "Standard",       "strings": [ "B", "E", "A", "D", "G", "B", "E" ] },
-    { "type": "guitar",  "name": "Full step down", "strings": [ "A", "D", "G", "C", "F", "A", "D" ] },
-    { "type": "guitar",  "name": "Standard",       "strings": [ "F#", "B", "E", "A", "D", "G", "B", "E" ] },
-    { "type": "guitar",  "name": "Drop E",         "strings": [ "E", "B", "E", "A", "D", "G", "B", "E" ] },
-    { "type": "guitar",  "name": "Standard",       "strings": [ "C#", "F#", "B", "E", "A", "D", "G", "B", "E" ] },
-  
-    { "type": "bass",    "name": "Standard",          "strings": [ "E", "A", "D", "G" ] },
-    { "type": "bass",    "name": "Drop D",            "strings": [ "D", "A", "D", "G" ] },
-    { "type": "bass",    "name": "Standard (low B)",  "strings": [ "B", "E", "A", "D", "G" ] },
-    { "type": "bass",    "name": "Standard (high C)", "strings": [ "E", "A", "D", "G", "C" ] },
-    { "type": "bass",    "name": "Standard",          "strings": [ "B", "E", "A", "D", "G", "C" ] },
-  
-    { "type": "ukelele", "name": "Standard",       "strings": [ "G", "C", "E", "A" ] },
-    { "type": "ukelele", "name": "Guitar",         "strings": [ "D", "G", "B", "E" ] },
-    { "type": "ukelele", "name": "D tuning",       "strings": [ "A", "D", "F#", "B" ] }
-  ]
-  */
+    };
