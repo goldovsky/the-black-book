e2649abea988978f0983b6982383af0c05401254
Quentin GOLDOVSKY <quentin.goldovsky@sii.fr>
2022-03-28 15:43:52 +0200

    ViewSettings display flex

--- src/components/ui/BaseSelect.vue
+++ src/components/ui/BaseSelect.vue
@@ -36,14 +36,41 @@ export default {
     },
   },
   beforeMount() {
-      if (this.initialValue !== undefined) {
-          this.selectedOption = this.initialValue;
-      } else if (Object.keys(this.options).length > 0) {
-      this.selectedOption = this.valueInsteadOfKey ? this.options[0] : Object.keys(this.options)[0];
+    if (this.initialValue !== undefined) {
+      this.selectedOption = this.initialValue;
+    } else if (Object.keys(this.options).length > 0) {
+      this.selectedOption = this.valueInsteadOfKey
+        ? this.options[0]
+        : Object.keys(this.options)[0];
     }
   },
 };
 </script>
 
-<style>
+<style scoped>
+select {
+  font-weight: bold;
+  background-color: white;
+  color: gray;
+  padding: 10px;
+  cursor: pointer;
+  border: none;
+  border-radius: 20px;
+  appearance: none;
+  outline: 0;
+  text-align: center;
+}
+
+option {
+  font-weight: bold;
+  /* text-align: left; */
+  color: var(--select-color-active);
+  background-color: var(--select-background-color-active);
+}
+
+select:hover,
+select:active {
+  color: var(--select-color-active);
+  background-color: var(--select-background-color-active);
+}
 </style>
--- src/components/views/ViewSettings.vue
+++ src/components/views/ViewSettings.vue
@@ -1,102 +1,125 @@
 <template>
   <div class="cardsettings">
     <h1>Settings</h1>
-    <base-card>
-      <h2>Instrument</h2>
-      <ul>
-        <li>
-          <base-button
-            :mode="!switchToBass ? 'flat' : ''"
-            @click="switchInstrumentToBass(true)"
-            >Bass</base-button
-          >
-          <base-button
-            :mode="switchToBass ? 'flat' : ''"
-            @click="switchInstrumentToBass(false)"
-            >Guitar</base-button
-          >
-        </li>
-        <li>
-          <div>nbStrings |</div>
-          <base-input-number
-            v-model="nbStrings"
-            :min="switchToBass ? 4 : 6"
-            :max="switchToBass ? 6 : 8"
-            :value="nbStrings"
-            @input="updateNbStrings"
-          />
-        </li>
-        <li>
-          <div>Tuning</div>
-          <base-select
-            :options="tuningOptions"
-            :valueInsteadOfKey="false"
-            :initialValue="tuning"
-            @select="updateTuning"
-          />
-          <div>In</div>
-          <base-select
-            :options="tuningOptions[tuning]"
-            :valueInsteadOfKey="true"
-            :initialValue="tonality"
-            @select="updateTonality"
-          />
-        </li>
-        <li>
-          <div>Dexterity |</div>
-          <div>Right</div>
-          <div title="Dexterity">
-            <div
-              class="slideDexterity"
-              :style="{
-                background: `var(--switch-background-${
-                  leftDexterity ? 'left' : 'right'
-                })`,
-              }"
+    <div class="divbasecards">
+      <base-card class="individualbasecard">
+        <h2>Instrument</h2>
+        <ul>
+          <li>
+            <base-button
+              :mode="!switchToBass ? 'flat' : ''"
+              @click="switchInstrumentToBass(true)"
+              >Bass</base-button
             >
-              <input
-                type="checkbox"
-                value="None"
-                id="slideDexterity"
-                name="check"
-                v-model="leftDexterity"
-                @click="switchDexterity"
-              />
-              <label for="slideDexterity"></label>
+            <base-button
+              :mode="switchToBass ? 'flat' : ''"
+              @click="switchInstrumentToBass(false)"
+              >Guitar</base-button
+            >
+          </li>
+          <li>
+            <div>nbStrings |</div>
+            <base-input-number
+              v-model="nbStrings"
+              :min="switchToBass ? 4 : 6"
+              :max="switchToBass ? 6 : 8"
+              :value="nbStrings"
+              @input="updateNbStrings"
+            />
+          </li>
+          <li>
+            <div>Tuning</div>
+            <base-select
+              :options="tuningOptions"
+              :valueInsteadOfKey="false"
+              :initialValue="tuning"
+              @select="updateTuning"
+            />
+            <div>|</div>
+            <base-select
+              :options="tuningOptions[tuning]"
+              :valueInsteadOfKey="true"
+              :initialValue="tonality"
+              @select="updateTonality"
+            />
+          </li>
+        </ul>
+      </base-card>
+      <base-card class="individualbasecard">
+        <h2>Diagrams</h2>
+        <ul>
+          <li>
+            <div>Chord Diagrams</div>
+            <base-button @click="switchChordDiagramOrientation">{{
+              chordDiagramHorizontal ? "Horizontal" : "Vertical"
+            }}</base-button>
+          </li>
+          <li>
+            <div>Fretboard Diagram</div>
+            <base-button @click="switchFretboardDiagramOrientation">{{
+              fretboardDiagramHorizontal ? "Horizontal" : "Vertical"
+            }}</base-button>
+          </li>
+          <li>
+            <div>Dexterity |</div>
+            <div>Right</div>
+            <div title="Dexterity">
+              <div
+                class="slideDexterity"
+                :style="{
+                  background: `var(--switch-background-${
+                    leftDexterity ? 'left' : 'right'
+                  })`,
+                }"
+              >
+                <input
+                  type="checkbox"
+                  value="None"
+                  id="slideDexterity"
+                  name="check"
+                  v-model="leftDexterity"
+                  @click="switchDexterity"
+                />
+                <label for="slideDexterity"></label>
+              </div>
             </div>
-          </div>
-          <div>Left</div>
-        </li>
-      </ul>
-    </base-card>
-    <base-card>
-      <ul>
-        <div>
-          <span>Diagrams Orientations</span>
-          <li>Chords | Horizontal | Vertical</li>
-          <li>Fretboard | Horizontal | Vertical</li>
-        </div>
-        <li>Setting 2</li>
-        <li>
-          <span>Theme</span>
-          <!-- theme switcher -->
-          <input
-            type="checkbox"
-            id="theme-switch"
-            class="theme-switch"
-            v-model="darkMode"
-          />
-          <label for="theme-switch" @click="switchTheme">
-            <span v-if="darkMode">
-              <img alt="logo" src="./../../assets/images/sun.png" width="40" />
-            </span>
-            <span v-else>
-              <img alt="logo" src="./../../assets/images/moon.png" width="40" />
-            </span>
-          </label>
-        </li>
-      </ul>
-    </base-card>
+            <div>Left</div>
+          </li>
+        </ul>
+      </base-card>
+      <base-card class="individualbasecard">
+        <h2>App</h2>
+        <ul>
+          <li>
+            <span>Theme</span>
+            <!-- theme switcher -->
+            <input
+              type="checkbox"
+              id="theme-switch"
+              class="theme-switch"
+              v-model="darkMode"
+            />
+            <label for="theme-switch" @click="switchTheme">
+              <span v-if="darkMode">
+                <img
+                  alt="logo"
+                  src="./../../assets/images/sun.png"
+                  width="40"
+                />
+              </span>
+              <span v-else>
+                <img
+                  alt="logo"
+                  src="./../../assets/images/moon.png"
+                  width="40"
+                />
+              </span>
+            </label>
+          </li>
+        </ul>
+      </base-card>
+      
+    </div>
   </div>
 </template>
 
@@ -105,13 +128,16 @@
  * TODO
  * at first page loading, icon get set to dark whatever the current theme is. check created method
  * @1) not sure it is the best way, but as we associate the value at created we might need to do something so it is updated as the store change state
- * @2 switch tuning to bass ones (1: change nbStrings ? should be automatic, 2: lload new tunings and update the selects)
+ * @2) switch tuning to bass ones (1: change nbStrings ? should be automatic, 2: lload new tunings and update the selects)
+ * @3) do the switch in store
  */
 export default {
   data() {
     return {
       darkMode: null,
       leftDexterity: null,
+      chordDiagramHorizontal: false,
+      fretboardDiagramHorizontal: true,
       switchToBass: null,
       nbStrings: 6,
       tuning: "",
@@ -136,7 +162,6 @@ export default {
   },
   watch: {
     tuning() {
-      console.log("tuning watcher");
       if (
         this.tuning !== undefined &&
         Object.keys(this.tuningOptions).length > 0
@@ -164,6 +189,14 @@ export default {
       });
       this.switchToBass = !this.switchToBass; // @1
       // @2
+    },
+    switchChordDiagramOrientation() {
+      //@3
+      this.chordDiagramHorizontal = !this.chordDiagramHorizontal;
+    },
+    switchFretboardDiagramOrientation() {
+      //@3
+      this.fretboardDiagramHorizontal = !this.fretboardDiagramHorizontal;
     },
     updateTuning(value) {
       this.tuning = value;
@@ -225,7 +258,6 @@ ul {
   justify-content: center;
   align-items: center;
 }
-
 li {
   margin: 0 0.5rem;
   display: flex;
@@ -233,8 +265,14 @@ li {
   align-items: center;
   justify-content: center;
 }
-
 .cardsettings {
   padding: 0rem 3rem 0rem 3rem;
+}
+.divbasecards {
+  display: flex;
+  flex-wrap: wrap;
+}
+.individualbasecard {
+  width: 40%;
 }
 </style>
