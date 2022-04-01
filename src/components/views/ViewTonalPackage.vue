cbc1a789b9409f49464a11b5b81fe2ffbf5c0f25
Quentin GOLDOVSKY <quentin.goldovsky@sii.fr>
2022-04-01 13:57:42 +0200

    view(tonalPackage): nav implemented

--- src/components/views/ViewTonalPackage.vue
+++ src/components/views/ViewTonalPackage.vue
@@ -1,13 +1,50 @@
 <template>
   <h1>Tonal Package</h1>
+  <div class="fakeNav">
+    <a
+      v-for="(screenValue, screenKey) in display"
+      :key="screenKey"
+      :class="{ fakeNavActive: this.display[screenKey] }"
+      @click="switchScreen(screenKey)"
+      >{{ screenKey }}</a
+    >
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      display: {
+        tonal: false,
+        notes: false,
+        intervals: false,
+        scales: false,
+        chords: false,
+        keys: true,
+      },
+    };
+  },
+  methods: {
+    switchScreen(val) {
+      Object.keys(this.display).forEach((key) => {
+        this.display[key] = false;
+      });
+      this.display[val] = true;
+    },
+  },
+  computed: {},
+};
 </script>
 
-<style>
-
+<style scoped>
+.fakeNav {
+  font-weight: 700;
+}
+.fakeNav a {
+  margin: 10px;
+}
+.fakeNavActive {
+  color: var(--secondary-color);
+}
 </style>
