f93a095f101f96a4781e8734fba8fe4f36b9b3b8
Quentin GOLDOVSKY <quentin.goldovsky@sii.fr>
2023-01-24 11:15:16 +0100

    feat(): dynamic chord name

--- src/components/views/ViewChordsDiagrams.vue
+++ src/components/views/ViewChordsDiagrams.vue
@@ -3,7 +3,7 @@
     <h1>Diagram</h1>
     <div class="chorddiagramparentdiv">
         <chord-diagram
-          name="G"
+          :name="chordName"
           nut-position="0"
           :chord="chord"
         ></chord-diagram>
@@ -42,19 +42,25 @@ export default {
   data() {
     return {
       dataChords: DATA_CHORDS,
-      chord: {}
+      chord: {},
+      chordName: ""
     };
   },
   created() {
     // G
-    this.setChord(this.dataChords['tuning_standard']['nb_strings_6']['root_on_string_6']['major']['caged_g']);
+    //this.setChord(this.dataChords['tuning_standard']['nb_strings_6']['root_on_string_6']['major']['caged_g']);
     // A
-    this.setChord(this.dataChords['tuning_standard']['nb_strings_6']['root_on_string_5']['major']['caged_a']);
-    //this.setChord(this.dataChords['tuning_standard']['nb_strings_6']['root_on_string_5']['major']['caged_a_sus_4']);
+    //this.setChord(this.dataChords['tuning_standard']['nb_strings_6']['root_on_string_5']['major']['caged_a']);
+    // Asus4
+    this.setChord(this.dataChords['tuning_standard']['nb_strings_6']['root_on_string_5']['major']['caged_a_sus_4']);
+    this.setChordName('Asus4');
   },
   methods: {
     setChord(chord) {
       this.chord = chord;
+    },
+    setChordName(name) {
+      this.chordName = name;
     }
   }
 };
