cea9b4c3ed0a4ce37271d7c2eac013ed64d7adac
Quentin GOLDOVSKY <quentin.goldovsky@sii.fr>
2023-10-27 16:41:30 +0200

    feat(): clean up scales.js

--- src/database/scales.js
+++ src/database/scales.js
@@ -1,30 +1,15 @@
 /**
- * USAGE
- * import DATA_SCALES from './.../data/scales.js';
- * 
- * data() {
-    return {
-      dataScales: DATA_SCALES,
-    };
-  },
- */
-
-/**
  * LEVELS
  * 1 : nb notes
  * 2 : scale
  * 3 : modes | intervals
- */
 
-/**
  * TODO
  * - in pentatonic add japanese scale
  * 
  * use an attribute to describe each chords build from each note?
  * or use chordIdentifier.js to determine it dynamically
- */
 
-/*
   nb of notes by scale
 
   | notes | name                  |
@@ -150,8 +135,8 @@ export default {
         "harmonic minor/Aeolian #7",
         "Locrian ♮6",
         "Ionian ♯5/Augmented major",
-        "Ukrainian Dorian scale",
-        "Phrygian dominant scale",
+        "Romanian / Ukrainian Dorian scale",
+        "Phrygian dominant / freygish",
         "Lydian ♯2",
         "Superlocrian double flat7/Altered diminished",
       ],
@@ -166,12 +151,10 @@ export default {
     "blues": [0, 3, 5, 6, 7, 10],
     "byzantine / double harmonic": [0, 1, 4, 5, 7, 8, 11],
     "chinese": [0, 4, 6, 7, 11],
-    "augmented": [0, 3, 4, 7, 8, 11],
     "diminished / arabian": [0, 2, 3, 5, 6, 8, 9, 11],
     "diminished (half whole / dominant)": [0, 1, 3, 4, 6, 7, 9, 10],
     "enigmatic": [0, 1, 4, 6, 8, 10, 11],
     "han kumoi": [0, 2, 5, 7, 8 ],
-    "harmonic minor": [0, 2, 3, 5, 7, 8, 11],
     "hatakambari": [0, 1, 4, 5, 7, 10, 11],
     "hijaz": [0, 1, 4, 5, 7, 8, 10],
     "hirajoshi": [0, 2, 3, 7, 8],
@@ -180,14 +163,9 @@ export default {
     "indian (raga asavari) descending": [0, 1, 3, 5, 7, 8, 10],
     "iwato": [0, 1, 5, 6, 10],
     "kokin-joshi": [0, 1, 5, 7, 10],
-    "lydian augmented": [0, 2, 4, 6, 8, 9, 11],
     "mongolian": [0, 2, 4, 7, 9],
     "oriental": [0, 1, 4, 5, 6, 9, 10],
     "persian": [0, 1, 4, 5, 6, 8, 11],
-    "phrygian": [0, 1, 3, 5, 7, 8, 10],
-    "phrygian dominant / freygish": [0, 1, 4, 5, 7, 8, 10 ],
-    "prometheus": [0, 2, 4, 6, 9, 10],
     "prometheus neapolitan": [0, 1, 4, 6, 9, 10],
-    "romanian / ukranian dorian": [0, 2, 3, 6, 7, 9, 10],
   }
    */
